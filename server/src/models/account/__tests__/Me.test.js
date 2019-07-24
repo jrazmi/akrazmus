441fd67c17a908fe510b4995829b52f886a87eca
joshuamitchell <joshua@gpsimpact.com>
2019-07-24 15:50:43 -0500

    updating me tests for glob perms

--- server/src/models/account/__tests__/Me.test.js
+++ server/src/models/account/__tests__/Me.test.js
@@ -1,4 +1,5 @@
 import * as ctx from '../../../../tests/setup';
+import _ from 'lodash';
 
 beforeEach(async () => {
     return await ctx.before();
@@ -13,6 +14,7 @@ const ME_QUERY =`{
         first_name
         last_name
         email
+        globalPermissions
     }
 }`
 describe("ME", () => {
@@ -30,6 +32,32 @@ describe("ME", () => {
         // convert pk int id to string for gql response
         expect(response.data.me.id).toBe(user.id.toString());
         expect(response.data.me.first_name).toBe(user.first_name);
-    })
+    });
+    it('Me query should return empty array for global permissions when no permissions exist', async () => {
+        const user = await ctx.db('users').where({id:1}).first()
+        const response = await ctx.mockCall(ME_QUERY, {}, {currentUser:user})
+        // convert pk int id to string for gql response
+        expect(response.data.me.id).toBe(user.id.toString());
+        expect(response.data.me.first_name).toBe(user.first_name);
+        expect(response.data.me.globalPermissions.length).toBe(0);
+    });
+    it('it should return a list of global permissions for users with global permissions', async () => {
+        const user = await ctx.db('users').where({id:1}).first();
+        const addPerms = await ctx.db('global_permissions').insert({user_id: user.id, permission: "ADMIN"})
+        const response = await ctx.mockCall(ME_QUERY, {}, {currentUser:user})
+        // convert pk int id to string for gql response
+        expect(response.data.me.id).toBe(user.id.toString());
+        expect(response.data.me.first_name).toBe(user.first_name);
+        expect(_.includes(response.data.me.globalPermissions, "ADMIN")).toBe(true)
+    });
+    it('it should return normal data and throw an error when returning inaccurate permission enum type', async () => {
+        const user = await ctx.db('users').where({id:1}).first();
+        const addPerms = await ctx.db('global_permissions').insert({user_id: user.id, permission: "blah"})
+        const response = await ctx.mockCall(ME_QUERY, {}, {currentUser:user})
+        // convert pk int id to string for gql response
+        expect(response.data.me.id).toBe(user.id.toString());
+        expect(response.data.me.first_name).toBe(user.first_name);
+        expect(response.errors.length >= 1).toBe(true);
+    });
 
 })
